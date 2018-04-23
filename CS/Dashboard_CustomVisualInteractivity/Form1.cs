380100e5f62671160bc790589cad68315e1a1682
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_CustomVisualInteractivity/Form1.cs
@@ -0,0 +1,64 @@
+﻿using DevExpress.DashboardWin;
+using DevExpress.XtraEditors;
+using DevExpress.XtraPivotGrid;
+using DevExpress.DashboardCommon;
+using System.Windows.Forms;
+using DevExpress.DashboardCommon.ViewerData;
+using DevExpress.DataAccess;
+namespace Dashboard_CustomVisualInteractivity {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+            this.salesPersonTableAdapter.Fill(this.nwindDataSet.SalesPerson);
+            
+            dashboardViewer1.LoadDashboard(@"..\..\Data\Dashboard.xml");
+        }
+
+        private void dashboardViewer1_DashboardItemVisualInteractivity(object sender, 
+            DashboardItemVisualInteractivityEventArgs e) {
+            if (e.DashboardItemName == "gridDashboardItem1") {
+                e.SelectionMode = DashboardSelectionMode.Multiple;
+                e.SetDefaultSelection(e.Data.GetAxisPoints(DashboardDataAxisNames.DefaultAxis)[0]);
+            }
+            if (e.DashboardItemName == "chartDashboardItem1") {
+                e.TargetAxes.Add(DashboardDataAxisNames.ChartArgumentAxis);
+                e.TargetAxes.Add(DashboardDataAxisNames.ChartSeriesAxis);
+                e.EnableHighlighting = true;
+            }
+        }
+
+        private void dashboardViewer1_DashboardItemSelectionChanged(object sender, 
+            DashboardItemSelectionChangedEventArgs e) {
+            pivotGridControl1.BeginUpdate();
+            fieldCategoryName1.FilterValues.FilterType = PivotFilterType.Included;
+            fieldCategoryName1.FilterValues.Clear();
+            foreach (AxisPointTuple selectedElement in e.CurrentSelection) {
+                string category = selectedElement.GetAxisPoint().DimensionValue.Value.ToString();
+                fieldCategoryName1.FilterValues.Add(category);
+            }
+            pivotGridControl1.EndUpdate();
+        }
+
+        private void dashboardViewer1_DashboardItemClick(object sender, 
+            DashboardItemMouseActionEventArgs e) {
+            if (e.DashboardItemName == "chartDashboardItem1" && e.GetAxisPoint() != null) {
+                XtraForm form = new XtraForm();
+                form.Text = e.GetAxisPoint(DashboardDataAxisNames.ChartArgumentAxis).
+                    DimensionValue.Value.ToString() + " - " +
+                    e.GetAxisPoint(DashboardDataAxisNames.ChartSeriesAxis).
+                    DimensionValue.Value.ToString();
+                DataGrid grid = new DataGrid();
+                grid.Parent = form; grid.Dock = DockStyle.Fill;
+                grid.DataSource = e.GetUnderlyingData();
+                form.ShowDialog();
+                form.Dispose();
+            }
+        }
+
+        private void dashboardViewer1_DataLoading(object sender, DataLoadingEventArgs e) {
+            if (e.DataSourceComponentName == "dataSource1") {
+                e.Data = this.nwindDataSet.SalesPerson;
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to add custom interactivity to a dashboard displayed in the WinForms Viewer
+
+
+<p>The <a href="http://documentation.devexpress.com/#Dashboard/CustomDocument15348">WinForms Dashboard Viewer</a> allows you to add a custom interactivity to dashboards using the <a href="http://documentation.devexpress.com/#Dashboard/DevExpressDashboardWinDashboardViewer_DashboardItemVisualInteractivitytopic">DashboardItemVisualInteractivity</a> event.<br />This example demonstrates the following capabilities:<br />- The <a href="http://documentation.devexpress.com/#Dashboard/CustomDocument15150">Grid</a> dashboard item allows you to select categories and filter an external <a href="http://documentation.devexpress.com/#WindowsForms/clsDevExpressXtraPivotGridPivotGridControltopic">PivotGridControl</a> according to selected records.<br />- The <a href="http://documentation.devexpress.com/#Dashboard/CustomDocument14719">Chart</a> dashboard item highlights a bar under the mouse pointer and allows you to obtain underlying data by clicking this bar.</p>
+
+<br/>
+
+
